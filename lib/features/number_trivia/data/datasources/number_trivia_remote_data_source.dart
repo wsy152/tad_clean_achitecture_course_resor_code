5beeb89946d1e761a3358bad686f4f783f17561e
Edvaldo <35712346+wsy152@users.noreply.github.com>
2023-10-26 23:14:49 -0300

    adicionar teste

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -1,7 +1,30 @@
 import 'package:curso_tdd_clean_architecture_reso_coder/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract interface class NumberTriviaRemoteDataSource {
   Future<NumberTriviaModel> getConcreteNumberTrivia(int number);
 
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource{
+  final http.Client client;
+
+  NumberTriviaRemoteDataSourceImpl({required this.client});
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) {
+    client.get(Uri.http('http://numbersapi.com/$number'), headers: {
+        'Content_Type': 'application/json',
+      });
+   
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() {
+   
+    throw UnimplementedError();
+  }
+
 }
--- test/features/number_trivia/data/datasources/number_trivia_remote_data_source_test.dart
+++ test/features/number_trivia/data/datasources/number_trivia_remote_data_source_test.dart
@@ -1,7 +1,55 @@
+import 'package:curso_tdd_clean_architecture_reso_coder/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:http/http.dart' as http;
+
+import '../../../../fixtures/fixture_reader.dart';
+import 'number_trivia_remote_data_source_test.mocks.dart';
+// import 'package:mockito/annotations.dart';
+
+
+// @GenerateNiceMocks([MockHttpClient])
+
+@GenerateNiceMocks([MockSpec<http.Client>(as: #MockHttpClient)])
+
+// class MockHttpClient extends Mock implements http.Client{}
 
 void main() {
-  testWidgets('number trivia remote data source ...', (tester) async {
+
+  late NumberTriviaRemoteDataSourceImpl dataSource;
+  late MockHttpClient mockHttpClient;
+
+  setUp(() {
+    mockHttpClient = MockHttpClient();
+
+    dataSource = NumberTriviaRemoteDataSourceImpl(client:mockHttpClient);
+
+
+
+  });
+
+
+  group('getConcreteNumberTrivia ...', () async {
+    const tNumber = 1;
+   // when( dataSource.client);
+
+   test(
+   '''should perform a GET request on a URL with number being the 
+   endpoint and with application/json header''', () async {
+       when(mockHttpClient.get(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => http.Response(fixture('trivia.json'),200));
+
+          dataSource.getConcreteNumberTrivia(tNumber);
+
+          verify(mockHttpClient
+          .get(Uri.http('http://numbersapi.com/$tNumber'), headers: {
+        'Content_Type': 'application/json',
+      }));
+
+
+   });
 
   });
 }
